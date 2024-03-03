f3046ce468642066f0b1a6ff8552aff5647f3132
Grant Wilson <grantwilson38@gmail.com>
2024-03-03 07:45:38 -0600

    Enemy snakes are moving as bodies

--- main.py
+++ main.py
@@ -102,4 +102,14 @@ while gameOn:
                 scoreboard.game_over()
                 game_over_sound.play()
 
+    # Check for head-on collision between enemy snakes
+    for i in range(len(scoreboard.enemy_snakes)):
+        for j in range(i + 1, len(scoreboard.enemy_snakes)):
+            if scoreboard.enemy_snakes[i].head.distance(scoreboard.enemy_snakes[j].head) < 10:
+                # Remove the collided snakes from the game
+                scoreboard.enemy_snakes[i].reset()
+                scoreboard.enemy_snakes[j].reset()
+                scoreboard.enemy_snakes = [snake for snake in scoreboard.enemy_snakes if snake != scoreboard.enemy_snakes[i] and snake != scoreboard.enemy_snakes[j]]
+                break
+
 screen.exitonclick()
